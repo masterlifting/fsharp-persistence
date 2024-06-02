9b8b91d9cf4190cee7a8365674f691a712fa27b1
Andrey Pestunov <masterlifting@gmail.com>
2024-06-02 17:03:47 +0200

    Refactor Persistence module to improve storage module names and types

--- src/Core.fs
+++ src/Core.fs
@@ -1,27 +1,28 @@
 module Persistence.Core
 
+open Infrastructure.Domain.Errors
+
 type Type =
     | FileSystem of string
     | InMemory
     | Database of string
 
-module Storage =
-    type Type =
-        | FileStorage of FileSystem.Storage
-        | MemoryStorage of InMemory.Storage
-        | DatabaseStorage of Database.Storage
+type Storage =
+    | FileStorage of FileSystem.Storage
+    | MemoryStorage of InMemory.Storage
+    | DatabaseStorage of Database.Storage
 
-    let create persistenceType =
-        match persistenceType with
-        | FileSystem path ->
-            match FileSystem.create path with
-            | Ok storage -> Ok <| FileStorage storage
-            | Error message -> Error message
-        | InMemory ->
-            match InMemory.create () with
-            | Ok storage -> Ok <| MemoryStorage storage
-            | Error message -> Error message
-        | Database connectionString ->
-            match Database.create connectionString with
-            | Ok storage -> Ok <| DatabaseStorage storage
-            | Error message -> Error message
+let getStorage persistenceType =
+    match persistenceType with
+    | FileSystem path ->
+        match FileSystem.create path with
+        | Ok storage -> Ok <| FileStorage storage
+        | Error message -> Error <| Persistence message
+    | InMemory ->
+        match InMemory.create () with
+        | Ok storage -> Ok <| MemoryStorage storage
+        | Error message -> Error <| Persistence message
+    | Database connectionString ->
+        match Database.create connectionString with
+        | Ok storage -> Ok <| DatabaseStorage storage
+        | Error message -> Error <| Persistence message
