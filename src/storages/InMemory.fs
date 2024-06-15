8b430c4acab271ff31607218450bcc11aaf75ba9
Andrey Pestunov <masterlifting@gmail.com>
2024-06-15 14:25:06 +0200

    Refactor storage module function signatures in InMemory.fs

--- src/storages/InMemory.fs
+++ src/storages/InMemory.fs
@@ -13,35 +13,31 @@ let internal create () =
     with ex ->
         Error ex.Message
 
-let add (cache: Context) =
-    fun key value ->
-        try
-            let _ = cache.TryAdd(key, value)
-            Ok()
-        with ex ->
-            Error <| Persistence ex.Message
+let add (cache: Context) key value =
+    try
+        let _ = cache.TryAdd(key, value)
+        Ok()
+    with ex ->
+        Error <| Persistence ex.Message
 
-let remove (cache: Context) =
-    fun key ->
-        try
-            let _ = cache.TryRemove(key)
-            Ok()
-        with ex ->
-            Error <| Persistence ex.Message
+let remove (cache: Context) key =
+    try
+        let _ = cache.TryRemove(key)
+        Ok()
+    with ex ->
+        Error <| Persistence ex.Message
 
-let update (cache: Context) =
-    fun key value ->
-        try
-            let _ = cache.TryUpdate(key, value, cache.[key])
-            Ok()
-        with ex ->
-            Error <| Persistence ex.Message
+let update (cache: Context) key value =
+    try
+        let _ = cache.TryUpdate(key, value, cache.[key])
+        Ok()
+    with ex ->
+        Error <| Persistence ex.Message
 
-let get (cache: Context) =
-    fun key ->
-        try
-            match cache.TryGetValue(key) with
-            | true, value -> Ok <| Some value
-            | _ -> Ok <| None
-        with ex ->
-            Error <| Persistence ex.Message
+let get (cache: Context) key =
+    try
+        match cache.TryGetValue(key) with
+        | true, value -> Ok <| Some value
+        | _ -> Ok <| None
+    with ex ->
+        Error <| Persistence ex.Message
