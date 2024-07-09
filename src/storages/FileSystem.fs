c7d94ed5513451b3f09420dc0b0f659918dc668e
Andrei Pestunov <masterlifting@gmail.com>
2024-07-09 22:45:30 +0200

    Refactor writeLine function in FileSystem.fs to handle cases where the stream is not writable

--- src/storages/FileSystem.fs
+++ src/storages/FileSystem.fs
@@ -9,23 +9,27 @@ type Context = FileStream
 
 let create path =
     try
-        let context = new Context(path, FileMode.OpenOrCreate, FileAccess.ReadWrite)
+        let context = new Context(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite)
         Ok context
     with ex ->
-        Error <| Operation { Message = ex.Message; Code = None }
+        Error <| NotSupported ex.Message
 
-let private writeLine data (stream: Context) =
+let rec private writeLine data (stream: Context) =
     async {
         try
-            let buffer = Encoding.UTF8.GetBytes(data + Environment.NewLine)
-            stream.Position <- stream.Length
-            do! stream.WriteAsync(buffer, 0, buffer.Length) |> Async.AwaitTask
-            do! stream.FlushAsync() |> Async.AwaitTask
+            if not stream.CanWrite then
+                do! Async.Sleep 500
+                return! writeLine data stream
+            else
+                let buffer = Encoding.UTF8.GetBytes(data + Environment.NewLine)
+                stream.Position <- stream.Length
+                do! stream.WriteAsync(buffer, 0, buffer.Length) |> Async.AwaitTask
+                do! stream.FlushAsync() |> Async.AwaitTask
 
-            stream.Close()
-            stream.Dispose()
+                stream.Close()
+                stream.Dispose()
 
-            return Ok()
+                return Ok()
         with ex ->
             stream.Close()
             stream.Dispose()
@@ -33,26 +37,30 @@ let private writeLine data (stream: Context) =
             return Error <| Operation { Message = ex.Message; Code = None }
     }
 
-let private readLines (stream: Context) =
+let rec private readLines (stream: Context) =
     async {
         try
-            let sb = StringBuilder()
-            let sr = new StreamReader(stream)
+            if not stream.CanRead then
+                do! Async.Sleep 500
+                return! readLines stream
+            else
+                let sb = StringBuilder()
+                let sr = new StreamReader(stream)
 
-            while not sr.EndOfStream do
-                let! line = sr.ReadLineAsync() |> Async.AwaitTask
+                while not sr.EndOfStream do
+                    let! line = sr.ReadLineAsync() |> Async.AwaitTask
 
-                match String.IsNullOrWhiteSpace line with
-                | false -> sb.AppendLine line |> ignore
-                | true -> ()
+                    match String.IsNullOrWhiteSpace line with
+                    | false -> sb.AppendLine line |> ignore
+                    | true -> ()
 
-            stream.Close()
-            stream.Dispose()
+                stream.Close()
+                stream.Dispose()
 
-            return
-                match sb.Length with
-                | 0 -> Error <| NotFound stream.Name
-                | _ -> Ok <| sb.ToString()
+                return
+                    match sb.Length with
+                    | 0 -> Error <| NotFound stream.Name
+                    | _ -> Ok <| sb.ToString()
         with ex ->
             stream.Close()
             stream.Dispose()
