94cabf86b5ac7675f3f7d5d2093eb3573f8e8c16
Andrei Pestunov <andrei.pestunov@akvelon.com>
2025-03-20 14:55:57 +0100

    Refactor translation handling: simplify item structure by removing Id, enhance hash-based lookups for response items

--- src/storages/filesystem/Command.fs
+++ src/storages/filesystem/Command.fs
@@ -7,3 +7,6 @@ open Infrastructure.SerDe
 module Json =
     let save<'a> (data: 'a array) client =
         data |> Json.serialize |> ResultAsync.wrap (Write.string client)
+
+    let save'<'a> (data: 'a array) options client =
+        data |> Json.serialize' options |> ResultAsync.wrap (Write.string client)
